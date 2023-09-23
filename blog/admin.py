eda38c20560f09107cf8fd3a1f104117549745c3
Annie <anniebosova@gmail.com>
2023-09-23 12:21:30 +0100

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post) #to make our model visible on the admin page,need to register the model
